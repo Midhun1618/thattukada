f364a6a02bfd04ab2b8723c34a147b3d17cde552
midhun1618 <midhumidhun342@gmail.com>
2025-04-23 15:54:13 +0530

    updated the cursor & font

--- src/Body.js
+++ src/Body.js
@@ -4,53 +4,35 @@ import './Body.css'
 function Body() {
   return (
     <div class="body-bg">
-    <div class="main-container">
-    <section class="card">
-      <h2 class="section-title">Live Orders</h2>
-      <div class="order-card">
-        <div>
-          <span>OrderId : </span><span class="font-bold">APR002</span>
-        </div>
-        <div>
-          <span class="tag">Snack</span>
-        </div>
+      <div class="main-container">
+        <section class="card">
+          <h2 class="section-title">Live Orders</h2>
+          <div class="order-card">
+            <div class="order-info">
+              <div>
+                <span>OrderId : </span><span class="font-bold">APR002</span>
+              </div>
+              <div>
+                <span class="tag">Snack</span>
+              </div>
+            </div>
+            <div class="order-info">
+              <div class="order-text">Items : 06</div>
+              <div class="order-text">Total Amount : Rs:149</div>
+            </div>
+          </div>
+
+        </section>
+
+        <section class="card">
+          <h2 class="section-title">Preparing Orders</h2>
+        </section>
+        <section class="card">
+          <h2 class="section-title">Ready to serve</h2>
+          
+        </section>
       </div>
-      <div class="order-text">Items : 06</div>
-      <div class="order-text">Total Amount : Rs:149</div>
-    </section>
-
-    <section class="card">
-      <h2 class="section-title">Current Orders</h2>
-    </section>
-
-    <div class="flex-col-gap">
-      
-      <section class="card">
-        <h2 class="section-title">Preparing</h2>
-        <div class="order-card">
-          <div>
-            <span>OrderId : </span><span class="font-bold">APR001</span>
-          </div>
-          <div>
-            <span class="tag">Snack</span>
-          </div>
-        </div>
-      </section>
-      <section class="card">
-        <h2 class="section-title">Ready to serve</h2>
-        <div class="order-card ready-card">
-          <div>
-            <span>OrderId : </span><span class="font-bold">APR008</span>
-          </div>
-          <div>
-            <span class="tag">Snack</span>
-          </div>
-        </div>
-      </section>
-
     </div>
-  </div>
-  </div>
   )
 }
 
